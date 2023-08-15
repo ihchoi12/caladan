1ff95f02a0155a8cf13edfebe3e47565a449d1e5
Nimish Wadekar <wadekar.nimish@gmail.com>
2023-08-15 15:43:59 +0000

    Add temporary hardcoded RESP commands

--- apps/synthetic/src/resp.rs
+++ apps/synthetic/src/resp.rs
@@ -1,10 +1,14 @@
 use clap::Arg;
+use rand::distributions::{Range, IndependentSample};
+use rand::{OsRng, Rng, random, thread_rng};
 
+use std::cell::RefCell;
 use std::cmp::min;
 use std::io;
 use std::io::{Error, ErrorKind, Read};
 use std::net::SocketAddrV4;
-use std::str::FromStr;
+use std::str::from_utf8;
+use std::sync::Arc;
 
 use Buffer;
 use Connection;
@@ -12,7 +16,24 @@ use LoadgenProtocol;
 use Packet;
 use Transport;
 
-#[derive(Copy, Clone)]
+static READ_COMMANDS: [&str; 4] = [
+    "PING",
+    "EXISTS numbers",
+    "LLEN numbers",
+    "LRANGE numbers 0 -1"
+];
+
+static WRITE_COMMANDS: [&str; 2] = [
+    "RPUSH numbers 9",
+    "LPOP numbers"
+];
+
+const RW_RANGE: usize = 0;
+const READ_RANGE: usize = 1;
+const WRITE_RANGE: usize = 2;
+
+const READ_PROBABILITY: u32 = 80;
+
 pub struct RespProtocol {
     
 }
@@ -33,19 +54,6 @@ impl RespProtocol {
     }
 }
 
-pub enum ParseState {
-    Done(usize, usize), // Header len, body len
-    Partial(usize, Option<usize>),
-}
-
-impl ParseState {
-    #[inline(always)]
-
-    pub fn new() -> ParseState {
-        ParseState::Partial(0, None)
-    }
-}
-
 #[inline(always)]
 fn find_subsequence(haystack: &[u8], needle: &[u8]) -> Option<usize> {
     haystack
@@ -53,42 +61,10 @@ fn find_subsequence(haystack: &[u8], needle: &[u8]) -> Option<usize> {
         .position(|window| window == needle)
 }
 
-fn header_extract(state: ParseState, buf: &[u8]) -> io::Result<ParseState> {
-    let (mut next_line_begin, mut content_len) = match state {
-        ParseState::Done(_, _) => return Ok(state),
-        ParseState::Partial(next_line_begin, content_len) => (next_line_begin, content_len),
-    };
-
-    if next_line_begin == buf.len() {
-        return Ok(ParseState::Partial(next_line_begin, content_len));
-    }
-
-    loop {
-        match find_subsequence(&buf[next_line_begin..], b"\r\n") {
-            Some(idx) => {
-                if idx == 0 {
-                    if content_len.is_none() {
-                        return Err(Error::new(ErrorKind::Other, "missing content-length"));
-                    }
-                    return Ok(ParseState::Done(next_line_begin + 2, content_len.unwrap()));
-                }
-                let header_ln = &buf[next_line_begin..idx + next_line_begin];
-
-                let utfln = unsafe { std::str::from_utf8_unchecked(&header_ln[..]) };
-                if next_line_begin == 0 {
-                    match header_ln[0] {
-                        b'+' | b'-' | b':' | b'$' | b'*' => (),
-                        _ => return Err(Error::new(ErrorKind::Other, format!("got bad RESP response: {}", utfln))),
-                    }
-                }
-
-                next_line_begin += idx + 2;
-            },
-
-            None => {
-                return Ok(ParseState::Partial(next_line_begin, content_len));
-            }
-        }
+fn check_good_response(buf: &[u8]) -> io::Result<()> {
+    match buf[0] {
+        b'+' | b'-' | b':' | b'$' | b'*' => Ok(()),
+        _ => Err(Error::new(ErrorKind::Other, format!("got bad RESP response: {}", from_utf8(buf).unwrap()))),
     }
 }
 
@@ -98,14 +74,23 @@ impl LoadgenProtocol for RespProtocol {
     }
 
     fn gen_req(&self, _i: usize, _p: &Packet, buf: &mut Vec<u8>) {
-        // PING
+        let mut rng = thread_rng();
+        let command = if rng.gen_range(0, 100) < READ_PROBABILITY {
+            READ_COMMANDS[rng.gen_range(0, READ_COMMANDS.len())]
+        } else {
+            WRITE_COMMANDS[rng.gen_range(0, WRITE_COMMANDS.len())]
+        };
 
-        buf.extend("*1\r\n$4\r\nPING\r\n".as_bytes());
+        let count = command.split(' ').count();
+        buf.extend(format!("*{count}\r\n").as_bytes());
+        for word in command.split(' ') {
+            buf.extend(format!("${}\r\n{}\r\n", word.len(), word).as_bytes());
+        }
+
+        //eprintln!("Sending: {}", from_utf8(buf).unwrap());
     }
 
     fn read_response(&self, mut sock: &Connection, buf: &mut Buffer) -> io::Result<(usize, u64)> {
-        let mut pstate = ParseState::new();
-
         if buf.data_size() == 0 {
             buf.try_shrink()?;
             let new_bytes = sock.read(buf.get_empty_buf())?;
@@ -115,43 +100,13 @@ impl LoadgenProtocol for RespProtocol {
             buf.push_data(new_bytes);
         }
 
-        loop {
-            let header_len;
-            let body_len;
+        check_good_response(buf.get_data())?;
 
-            match header_extract(pstate, buf.get_data())? {
-                ParseState::Done(hlen, blen) => {
-                    header_len = hlen;
-                    body_len = blen;
-                }
+        //eprintln!("Received: {}", from_utf8(buf.get_data()).unwrap());
 
-                ParseState::Partial(a, b) => {
-                    pstate = ParseState::Partial(a, b);
-                    buf.try_shrink()?;
-                    let new_bytes = sock.read(buf.get_empty_buf())?;
-                    if new_bytes == 0 {
-                        return Err(Error::new(ErrorKind::UnexpectedEof, "eof"));
-                    }
-                    buf.push_data(new_bytes);
-                    continue;
-                }
-            };
+        buf.pull_data(buf.data_size());
+        buf.try_shrink()?;
 
-            /* drain socket if needed */
-            let total_req_bytes = header_len + body_len;
-            let curbytes = buf.data_size();
-            buf.pull_data(min(curbytes, total_req_bytes));
-            buf.try_shrink()?;
-            if curbytes < total_req_bytes {
-                let mut to_read = total_req_bytes - curbytes;
-                while to_read > 0 {
-                    let rlen = min(to_read, buf.get_free_space());
-                    sock.read_exact(&mut buf.get_empty_buf()[..rlen])?;
-                    to_read -= rlen;
-                }
-            }
-
-            return Ok((0, 0));
-        }
+        return Ok((0, 0));
     }
 }
